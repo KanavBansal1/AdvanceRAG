45254f902d513282ed44a6e9aee83a5d31d2b7c7
KanavBansal1 <bansalkanav7@gmail.com>
2026-04-03 11:32:25 +0530

    fix loader import for deployment

--- ingestion/loader.py
+++ ingestion/loader.py
@@ -1,16 +1,24 @@
-from langchain_community.document_loaders import PyPDFLoader, TextLoader
 import os
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
+
 
 def load_documents(data_path):
-    
+
+    # Create folder if not exists
+    os.makedirs(data_path, exist_ok=True)
+
     documents = []
 
     for file in os.listdir(data_path):
+
         path = os.path.join(data_path, file)
-        if(file.endswith(".pdf")):
+
+        if file.endswith(".pdf"):
             loader = PyPDFLoader(path)
             documents.extend(loader.load())
-        elif(file.endswith(".txt")):
+
+        elif file.endswith(".txt"):
             loader = TextLoader(path)
             documents.extend(loader.load())
+
     return documents

f18811acbaf99bce33fb9b32b96455c7eca2a6b9
KanavBansal1 <bansalkanav7@gmail.com>
2026-04-03 11:55:08 +0530

    fix groq auth

--- llm/groq_llm.py
+++ llm/groq_llm.py
@@ -1,14 +1,13 @@
+import streamlit as st
 from langchain_groq import ChatGroq
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 def get_llm():
 
     llm = ChatGroq(
         temperature=0,
-        model_name="llama-3.1-8b-instant"
+        model_name="llama-3.1-8b-instant",
+        groq_api_key=st.secrets["GROQ_API_KEY"]
     )
 
     return llm
